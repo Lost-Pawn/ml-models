d0aa81df1a38887003fa0806445cb3138d6b3de5
Shashank Kumar Gupta <crushcastic@gmail.com>
2026-07-03 16:01:17 +0530

    Refactor .gitignore and update requirements.txt with specific package versions

--- models/house-predictor.py
+++ models/house-predictor.py
@@ -1,6 +1,10 @@
+import pandas as pd
+import numpy as np
 from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-housing = fetch_california_housing(name="housing", as_frame=True)
+housing = fetch_california_housing(as_frame=True)
 
 X = housing.data
 y = housing.target
